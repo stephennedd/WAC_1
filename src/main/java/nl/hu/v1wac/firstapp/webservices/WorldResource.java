c21e03b45db10fc2206ba5a0e62cf0f5c45bb227
Stephen MSI <veronika1>
2019-05-10 12:05:32 +0200

    3.1 tm 3.3 af

--- src/main/java/nl/hu/v1wac/firstapp/webservices/WorldResource.java
+++ src/main/java/nl/hu/v1wac/firstapp/webservices/WorldResource.java
@@ -2,6 +2,9 @@ package nl.hu.v1wac.firstapp.webservices;
 
 import java.util.List;
 
+import javax.json.*;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.*;
 import javax.ws.rs.Path;
 
@@ -11,31 +14,69 @@ import nl.hu.v1wac.firstapp.webservices.ServiceProvider;
 public class WorldResource {
 	
 	public WorldService ws = ServiceProvider.getWorldService();
+	
+	public JsonObjectBuilder tmplCountry(Country c) {
+		JsonObjectBuilder job = Json.createObjectBuilder();
+		
+		job.add("code", c.getCode());
+        job.add("name", c.getName());
+        job.add("capital", c.getCapital());
+        job.add("surface", c.getSurface());
+        job.add("government", c.getGovernment());
+        job.add("lat", c.getLatitude());
+        job.add("iso3", c.getIso3());
+        job.add("continent", c.getContinent());
+        job.add("region", c.getRegion());
+        job.add("population", c.getPopulation());
+        job.add("lng", c.getLongitude());
+	
+        return job;
+	}
 
 	@GET
 	@Produces("application/json")
-	public List<Country> allCountries() {
-		return ws.getAllCountries();
+	public String allCountries() {
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+		
+		for (Country c : ws.getAllCountries()) {
+			jab.add(tmplCountry(c));
+		}
+		JsonArray array = jab.build();
+		return array.toString();
 	}
 	
 	@GET
 	@Produces("application/json")
 	@Path("{code}")
-	public Country country(@PathParam("code") String code) {
-		return ws.getCountryByCode(code);
+	public String country(@PathParam("code") String code) {
+		Country c = ws.getCountryByCode(code);
+		
+		return tmplCountry(c).build().toString();
 	}
 	
 	@GET
 	@Path("/largestsurfaces")
 	@Produces("application/json")
-	public List<Country> getLargestSurfaces() {
-		return ws.get10LargestSurfaces();
+	public String getLargestSurfaces() {
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+				
+			for (Country c : ws.get10LargestSurfaces()) {
+				jab.add(tmplCountry(c));
+			}
+			JsonArray array = jab.build();
+			return array.toString();
 	}
 	
 	@GET
 	@Path("/largestpopulations")
 	@Produces("application/json")
-	public List<Country> getLargestPopulations() {
-		return ws.get10LargestPopulations();
+	public String getLargestPopulations() {
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+				
+			for (Country c : ws.get10LargestPopulations()) {
+				jab.add(tmplCountry(c));
+			}
+			JsonArray array = jab.build();
+			return array.toString();
 	}
 }
